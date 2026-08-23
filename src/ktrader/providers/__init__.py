4fff95f33efb8cfae21b2e4e20f3b14b4b316c86
kolemasakar <kolemasakar@gmail.com>
2026-08-23 12:13:35 +0300

    PHASE_3_PROVIDER_EXPORTS

--- src/ktrader/providers/__init__.py
+++ src/ktrader/providers/__init__.py
@@ -1,10 +1,12 @@
 from ktrader.providers.base import MarketDataProvider, ProviderError
 from ktrader.providers.binance_usdm import BinanceUSDMProvider
 from ktrader.providers.bybit_linear import BybitLinearProvider
+from ktrader.providers.live import LiveCandleEvent
 
 __all__ = [
     "MarketDataProvider",
     "ProviderError",
     "BinanceUSDMProvider",
     "BybitLinearProvider",
+    "LiveCandleEvent",
 ]
