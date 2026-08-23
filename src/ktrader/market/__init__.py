cf524bb8ab221d78402bfbc70df35ad6c0f22cc5
kolemasakar <kolemasakar@gmail.com>
2026-08-23 12:00:34 +0300

    PHASE_2_COMPAT_EXPORTS_FIX

--- src/ktrader/market/__init__.py
+++ src/ktrader/market/__init__.py
@@ -1,4 +1,17 @@
 from ktrader.market.bootstrap import BootstrapError, BootstrapResult, HistoryPlan, MTFBootstrapService
+from ktrader.market.service import (
+    ProviderFailure,
+    ProviderSelection,
+    UniverseSnapshot,
+    load_universe,
+    select_first_available_provider,
+)
+from ktrader.market.universe import (
+    UniverseCandidate,
+    UniverseConfig,
+    build_universe,
+    liquidity_score,
+)
 from ktrader.market.validation import CandleGap, CandleValidationError, FreshnessPolicy, FreshnessResult
 
 __all__ = [
@@ -6,6 +19,15 @@ __all__ = [
     "BootstrapResult",
     "HistoryPlan",
     "MTFBootstrapService",
+    "ProviderFailure",
+    "ProviderSelection",
+    "UniverseSnapshot",
+    "load_universe",
+    "select_first_available_provider",
+    "UniverseCandidate",
+    "UniverseConfig",
+    "build_universe",
+    "liquidity_score",
     "CandleGap",
     "CandleValidationError",
     "FreshnessPolicy",
