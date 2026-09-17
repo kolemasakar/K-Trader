f98b07cf926e7eaa1ed22ce5960c2abd60759b6e
kolemasakar <kolemasakar@gmail.com>
2026-09-17 13:26:22 +0300

    Export K_AI MT4 market-context adapter

--- src/ktrader/providers/__init__.py
+++ src/ktrader/providers/__init__.py
@@ -1,6 +1,7 @@
 from ktrader.providers.base import MarketDataProvider, ProviderError
 from ktrader.providers.binance_usdm import BinanceUSDMProvider
 from ktrader.providers.bybit_linear import BybitLinearProvider
+from ktrader.providers.kai_mt4 import KAIMT4MarketContextAdapter
 from ktrader.providers.live import LiveCandleEvent
 
 __all__ = [
@@ -8,5 +9,6 @@ __all__ = [
     "ProviderError",
     "BinanceUSDMProvider",
     "BybitLinearProvider",
+    "KAIMT4MarketContextAdapter",
     "LiveCandleEvent",
 ]
